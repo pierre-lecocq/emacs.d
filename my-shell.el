8f0ce0c352d231a446eaef8d942e67c9ffbaf3cf
Pierre Lecocq <pierre.lecocq@gmail.com>
2013-09-16 10:48:08 +0200

    Fix ShellPop fucking keys problems

--- my-shell.el
+++ my-shell.el
@@ -4,7 +4,7 @@
 
 (require 'shell-pop)
 
-(shell-pop-set-internal-mode "ansi-term")
+(shell-pop-set-internal-mode "shell")
 (shell-pop-set-internal-mode-shell "/bin/bash")
 (shell-pop-set-window-height 30)
 (shell-pop-set-window-position "bottom")
