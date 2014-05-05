908f79343add31c0fc0e0c0cd572f797696b396b
Pierre Lecocq <pierre.lecocq@gmail.com>
2014-05-05 14:57:19 +0200

    Configure org-capture

--- init.el
+++ init.el
@@ -6,17 +6,17 @@
 (require 'org-install)
 (require 'ob-tangle)
 
-(setq org-files-dir (format "%sconfig" user-emacs-directory))
-(add-to-list 'load-path org-files-dir)
+(setq org-files-config-dir (format "%sconfig" user-emacs-directory))
+(add-to-list 'load-path org-files-config-dir)
 
 ;; Set org config files to load
 
 (setq org-files (list
-         (format "%s/01-packages.org" org-files-dir)
-         (format "%s/02-common.org" org-files-dir)
-         (format "%s/03-modes.org" org-files-dir)
-         (format "%s/04-keybindings.org" org-files-dir)
-         (format "%s/09-%s.org" org-files-dir (downcase (car (split-string system-name "\\."))))))
+         (format "%s/01-packages.org" org-files-config-dir)
+         (format "%s/02-common.org" org-files-config-dir)
+         (format "%s/03-modes.org" org-files-config-dir)
+         (format "%s/04-keybindings.org" org-files-config-dir)
+         (format "%s/09-%s.org" org-files-config-dir (downcase (car (split-string system-name "\\."))))))
 
 ;; Load org config files
 
