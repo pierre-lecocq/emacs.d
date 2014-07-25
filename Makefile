b83cba32fbb4c403fac2da6291cb705e15989855
Pierre Lecocq <pierre.lecocq@gmail.com>
2014-07-25 14:22:07 +0200

    Separate org source files and elisp tangled files in directories

--- Makefile
+++ Makefile
@@ -2,14 +2,10 @@ all:
 	emacs -batch -l init.el
 
 compile:
-	emacs -batch -f batch-byte-compile init.el config/*.el
+	emacs -batch -f batch-byte-compile init.el config/elisp/*.el
 
 clean:
-	rm -f *.elc *~ config/*.el config/*.elc config/*~
+	rm -rf *.elc config/elisp
 
 reset: clean
 	rm -rf el-get
-
-eshell:
-	test -s ~/.eshell/alias || (mkdir ~/.eshell; echo "alias l ls -lh" > ~/.eshell/alias && echo "alias la ls -lhA" >> ~/.eshell/alias)
-#	test -s ~/.eshell/alias || (echo "Not exists" && mkdir ~/.eshell; alias | tr -d "'" |  sed  "s/^alias \(.*\)=/alias \1 /g" > ~/.eshell/alias)
--- init.el
+++ init.el
@@ -1,34 +1,46 @@
 ;;
 ;; Emacs config file - Pierre Lecocq
-;; Load all configs from org files code blocks (~/.emacs.d/config/*.org)
+;; Load all configs from org files code blocks (~/.emacs.d/config/org/*.org)
 ;;
+
+;; Prepare directories
+
+(setq config-dir-org (format "%sconfig/org" user-emacs-directory))
+(unless (file-exists-p config-dir-org)
+  (error (format "Missing %s directory" config-dir-org)))
+
+(setq config-dir-elisp (format "%sconfig/elisp" user-emacs-directory))
+(unless (file-exists-p config-dir-elisp)
+  (make-directory config-dir-elisp))
+
+;; Set load path
+
+(add-to-list 'load-path config-dir-elisp)
+
+;; Set org config files to load
+
+(setq org-files
+      (list (format "%s/01-packages.org" config-dir-org)
+            (format "%s/02-common.org" config-dir-org)
+            (format "%s/03-modes.org" config-dir-org)
+            (format "%s/04-keybindings.org" config-dir-org)
+            (format "%s/09-%s.org" config-dir-org (downcase (car (split-string system-name "\\."))))))
+
+;; Load org config files
 
 (require 'org-install)
 (require 'ob-tangle)
 
-(setq org-files-config-dir (format "%sconfig" user-emacs-directory))
-(add-to-list 'load-path org-files-config-dir)
-
-;; Set org config files to load
-
-(setq org-files (list
-         (format "%s/01-packages.org" org-files-config-dir)
-         (format "%s/02-common.org" org-files-config-dir)
-         (format "%s/03-modes.org" org-files-config-dir)
-         (format "%s/04-keybindings.org" org-files-config-dir)
-         (format "%s/09-%s.org" org-files-config-dir (downcase (car (split-string system-name "\\."))))))
-
-;; Load org config files
-
 (mapc (lambda (org-file)
-        (setq el-file (replace-regexp-in-string "\\.org$" ".el" org-file))
-        (unless (file-exists-p el-file)
-          (if (file-exists-p org-file)
-              (progn
-                (condition-case nil
-                    (org-babel-load-file org-file)
-                  (error "Error while loading code from %s" org-file)))
-            (error "Can not load config file %s" org-file)))
-        (load el-file)) org-files)
+        (let ((el-file (replace-regexp-in-string "/org/\\([-_0-9A-Za-z]+\\)\\.org" "/elisp/\\1.el" org-file)))
+          (unless (file-exists-p el-file)
+            (if (file-exists-p org-file)
+                (progn
+                  (condition-case nil
+                      (org-babel-tangle-file org-file el-file "emacs-lisp")
+                    (error "Error while loading code from %s" org-file)))
+              (error "Can not load config file %s" org-file)))
+          (load-file el-file))
+        ) org-files)
 
 ;; EOF
