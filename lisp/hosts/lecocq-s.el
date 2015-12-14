f5b68e45a00670e3d29282af1b4dd3c8faf22595
Pierre Lecocq <pierre.lecocq@gmail.com>
2015-12-14 14:57:10 +0100

    Remove debug

--- lisp/hosts/lecocq-s.el
+++ lisp/hosts/lecocq-s.el
@@ -1,12 +1,10 @@
 ;;; lecocq-s.el --- Emacs configuration - lecocq-s host
 
-;; Time-stamp: <2015-12-14 14:54:35>
+;; Time-stamp: <2015-12-14 14:56:53>
 ;; Copyright (C) 2015 Pierre Lecocq
 
 ;;; Commentary:
 
 ;;; Code:
-
-(message "HEYYY !!")
 
 ;;; lecocq-s.el ends here
