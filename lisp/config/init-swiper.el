255030d16d87c2849dc87359d28e399b6f114ba4
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-01-12 10:43:00 +0100

    Swiper configuration

--- lisp/config/init-swiper.el
+++ lisp/config/init-swiper.el
@@ -1,6 +1,6 @@
 ;;; init-swiper.el --- Emacs configuration - swiper
 
-;; Time-stamp: <2016-01-12 10:28:08>
+;; Time-stamp: <2016-01-12 10:38:45>
 ;; Copyright (C) 2016 Pierre Lecocq
 
 ;;; Commentary:
@@ -8,7 +8,10 @@
 ;;; Code:
 
 (use-package swiper :ensure t
-  :init (ivy-mode))
+  :init (progn
+          (ivy-mode 1)
+          (setq ivy-use-virtual-buffers t))
+  :bind (("C-s" . swiper)))
 
 (provide 'init-swiper)
 
