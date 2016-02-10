4c16cc000c24d9dd00a342d82e06cf5b4eceb761
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-02-10 09:54:07 +0100

    Add other bookmark

--- lisp/config-bookmark.el
+++ lisp/config-bookmark.el
@@ -1,6 +1,6 @@
 ;;; config-bookmark.el --- Emacs config - bookmark
 
-;; Time-stamp: <2016-01-20 09:03:33>
+;; Time-stamp: <2016-02-10 09:53:51>
 ;; Copyright (C) 2015 Pierre Lecocq
 
 ;;; Commentary:
@@ -14,7 +14,8 @@
       bookmark-alist `(("Home"          (filename . "~/"))
                        ("Sources"       (filename . "~/src"))
                        ("Emacs"         (filename . "~/src/emacs.d"))
-                       ("Fotolia"       (filename . ,(-secret-path-ftl "/www/fotolia")))))
+                       ("Fotolia"       (filename . ,(-secret-path-ftl "/www/fotolia")))
+                       ("AdobeStock"    (filename . ,(-secret-path-adb "/www/adobestock")))))
 
 (provide 'config-bookmark)
 
