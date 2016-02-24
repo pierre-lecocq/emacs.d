b5e6537a95409a52a245c42d8da7d5bd1cbee27d
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-02-24 10:00:39 +0100

    Final newsticker config

--- lisp/config-newsticker.el
+++ lisp/config-newsticker.el
@@ -1,11 +1,18 @@
 ;;; config-newsticker.el --- Emacs configuration - newsticker
 
-;; Time-stamp: <2016-02-24 09:33:39>
+;; Time-stamp: <2016-02-24 09:58:26>
 ;; Copyright (C) 2016 Pierre Lecocq
 
 ;;; Commentary:
 
 ;;; Code:
+
+(defun pl-launch-newsticker ()
+  "Launch newsticker."
+  (interactive)
+  (unless (newsticker-running-p)
+    (newsticker-start))
+  (newsticker-treeview))
 
 (use-package w3m :ensure t
   :commands w3m-region)
@@ -17,16 +24,22 @@
                 newsticker-dir (concat files-dir "newsticker")
                 newsticker-cache-filename (concat files-dir "newsticker" "/.cache")
                 newsticker-url-list-defaults nil
-                newsticker-url-list '(("Planet Emacsen" "http://planet.emacsen.org/atom.xml")
-                                      ("/r/lisp ""http://www.reddit.com/r/lisp.rss")
-                                      ("/r/debian ""http://www.reddit.com/r/debian.rss")
-                                      ("/r/emacs" "http://www.reddit.com/r/emacs.rss")
-                                      ("/r/netsec" "http://www.reddit.com/r/netsec.rss")
-                                      ("/r/linux" "http://www.reddit.com/r/linux.rss")
-                                      ("/r/ruby" "http://www.reddit.com/r/ruby.rss")
-                                      ("XKCD" "http://xkcd.com/rss.xml")))
-          (newsticker-start))
-  :bind (("C-c r" . newsticker-treeview)))
+                newsticker-url-list '(("p-emacsen" "http://planet.emacsen.org/atom.xml")
+                                      ("r-lisp" "http://www.reddit.com/r/lisp.rss")
+                                      ("r-debian" "http://www.reddit.com/r/debian.rss")
+                                      ("r-emacs" "http://www.reddit.com/r/emacs.rss")
+                                      ("r-netsec" "http://www.reddit.com/r/netsec.rss")
+                                      ("r-linux" "http://www.reddit.com/r/linux.rss")
+                                      ("r-ruby" "http://www.reddit.com/r/ruby.rss")
+                                      ("xkcd" "http://xkcd.com/rss.xml")
+                                      ("commit-strip" "http://www.commitstrip.com/fr/feed/")))
+          (setq newsticker-groups-filename (concat files-dir "newsticker" "/.groups")
+                newsticker-groups '("Feeds"
+                                    ("System" "r-linux" "r-debian" "r-netsec")
+                                    ("Emacs" "p-emacsen" "r-emacs")
+                                    ("Dev" "r-lisp" "r-ruby")
+                                    ("Other" "xkcd" "commit-strip"))))
+  :bind (("C-c r" . pl-launch-newsticker)))
 
 (provide 'config-newsticker)
 
