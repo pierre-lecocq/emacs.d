c0f633a0c1a6fd09bbf47b52423c519b916ec44d
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-02-24 09:34:25 +0100

    better newsticker configuration

--- lisp/config-newsticker.el
+++ lisp/config-newsticker.el
@@ -1,6 +1,6 @@
 ;;; config-newsticker.el --- Emacs configuration - newsticker
 
-;; Time-stamp: <2016-02-24 08:39:18>
+;; Time-stamp: <2016-02-24 09:33:39>
 ;; Copyright (C) 2016 Pierre Lecocq
 
 ;;; Commentary:
@@ -14,9 +14,17 @@
   :init (progn
           (setq newsticker-retrieval-interval 600 ;; 10mins
                 newsticker-html-renderer 'w3m-region
+                newsticker-dir (concat files-dir "newsticker")
+                newsticker-cache-filename (concat files-dir "newsticker" "/.cache")
                 newsticker-url-list-defaults nil
-                newsticker-url-list '(("Planet Emacsen" "http://planet.emacsen.org/atom.xml" nil nil nil)
-                                      ("XKCD" "http://xkcd.com/rss.xml" nil nil nil)))
+                newsticker-url-list '(("Planet Emacsen" "http://planet.emacsen.org/atom.xml")
+                                      ("/r/lisp ""http://www.reddit.com/r/lisp.rss")
+                                      ("/r/debian ""http://www.reddit.com/r/debian.rss")
+                                      ("/r/emacs" "http://www.reddit.com/r/emacs.rss")
+                                      ("/r/netsec" "http://www.reddit.com/r/netsec.rss")
+                                      ("/r/linux" "http://www.reddit.com/r/linux.rss")
+                                      ("/r/ruby" "http://www.reddit.com/r/ruby.rss")
+                                      ("XKCD" "http://xkcd.com/rss.xml")))
           (newsticker-start))
   :bind (("C-c r" . newsticker-treeview)))
 
