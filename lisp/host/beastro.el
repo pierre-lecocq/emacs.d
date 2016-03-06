635c813bf325d8aeb27d9a6631b0a675e8d9e92f
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-03-06 10:48:19 +0100

    Add beastro host file

---
+++ lisp/host/beastro.el
@@ -0,0 +1,13 @@
+;;; host-beastro.el --- Emacs config - beastro
+
+;; Time-stamp: <2016-03-06 10:47:41>
+;; Copyright (C) 2015 Pierre Lecocq
+
+;;; Commentary:
+
+;;; Code:
+
+(when (display-graphic-p)
+  (pl-transparency 90))
+
+;;; host-beastro.el ends here
