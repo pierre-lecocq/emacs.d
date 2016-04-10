db064bf74a61f12864c71d8bef6dffd218c22d82
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-04-10 17:33:36 +0200

    Slime configuration

--- lisp/config/pl-lisp.el
+++ lisp/config/pl-lisp.el
@@ -1,6 +1,6 @@
 ;;; pl-lisp.el --- Emacs configuration - lisp
 
-;; Time-stamp: <2016-02-29 00:02:01>
+;; Time-stamp: <2016-04-10 17:33:04>
 ;; Copyright (C) 2015 Pierre Lecocq
 
 ;;; Commentary:
@@ -11,9 +11,16 @@
 
 (use-package slime :ensure t
   :init (progn
+          ;; Assuming (ql:quickload "quicklisp-slime-helper") has be ran in quicklisp before
+          (let ((helper-file (expand-file-name "~/quicklisp/slime-helper.el")))
+            (if (file-exists-p helper-file)
+                (load helper-file)
+              (warn "(ql:quickload \"quicklisp-slime-helper\") must be run in quicklisp before")))
+
           (if (eq system-type 'darwin)
               (setq inferior-lisp-program "/usr/local/bin/sbcl")
             (setq inferior-lisp-program "sbcl"))
+
           (slime-setup '(slime-company))))
 
 (provide 'pl-lisp)
