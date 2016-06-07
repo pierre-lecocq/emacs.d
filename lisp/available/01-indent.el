c41cabd11f0f49e0af27416a4757d80689aad542
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-06-07 16:33:11 +0200

    Fix indent

--- lisp/available/01-indent.el
+++ lisp/available/01-indent.el
@@ -1,6 +1,6 @@
 ;;; 01-indent.el --- Indentation
 
-;; Time-stamp: <2016-02-29 00:01:42>
+;; Time-stamp: <2016-06-07 16:30:50>
 ;; Copyright (C) 2015 Pierre Lecocq
 
 ;;; Commentary:
@@ -8,11 +8,8 @@
 ;;; Code:
 
 (setq-default indent-tabs-mode nil
-	      tab-width 4
+              tab-width 4
               c-basic-offset 4
               c-hanging-comment-ender-p nil)
-
-(c-set-offset 'arglist-intro '+)
-(c-set-offset 'arglist-close '0)
 
 ;;; 01-indent.el ends here
