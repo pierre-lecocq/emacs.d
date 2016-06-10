63afe8595eb10fb6dbf503df25bc70a7e777bcf5
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-06-10 16:25:18 +0200

    Working tags on osx

--- lisp/available/01-tags.el
+++ lisp/available/01-tags.el
@@ -1,21 +1,23 @@
 ;;; 01-tags.el --- Tags
 
-;; Time-stamp: <2016-06-10 15:55:38>
+;; Time-stamp: <2016-06-10 16:24:46>
 ;; Copyright (C) 2016 Pierre Lecocq
 
 ;;; Commentary:
 
 ;;; Code:
 
+(defvar tags-bin-path "/usr/local/bin/ctags")
+
 (defun pl-files-regexp-by-mode ()
   "Get files regexp by mode."
   (cond
-   ((string= major-mode "common-lisp-mode") "cl")
-   ((string= major-mode "emacs-lisp-mode") "el")
-   ((string= major-mode "c-mode") "[ch]")
-   ((string= major-mode "ruby-mode") "rb")
-   ((string= major-mode "pythome-mode") "py")
-   ((string= major-mode "php-mode") "php")))
+   ((string= major-mode "common-lisp-mode") ".cl")
+   ((string= major-mode "emacs-lisp-mode") ".el")
+   ((string= major-mode "c-mode") ".c.h")
+   ((string= major-mode "ruby-mode") ".rb")
+   ((string= major-mode "pythome-mode") ".py")
+   ((string= major-mode "php-mode") ".php.css.js")))
 
 (defun pl-compile-tags (directory)
   "Compile etags for a given DIRECTORY."
@@ -23,8 +25,7 @@
   (let* ((dir (expand-file-name (file-name-as-directory directory)))
          (dir-local (replace-regexp-in-string "/[^/]+:[^/]+:/" "/" dir)))
     (cd dir)
-    (compile (format "find . -name \"*.%s\" -type f -print | etags -" (pl-files-regexp-by-mode)))
-    (bury-buffer "*compilation*")
+    (compile (format "%s -e -h \"%s\" -R ." tags-bin-path (pl-files-regexp-by-mode)))
     (let ((file (concat dir "/TAGS")))
       (setq tags-file-name file)
       (visit-tags-table file))))
