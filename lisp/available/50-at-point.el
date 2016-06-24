d3d7b3dd1ff37e54819f1850784006547895393b
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-06-24 11:32:01 +0200

    Add at-point lib

---
+++ lisp/available/50-at-point.el
@@ -0,0 +1,37 @@
+;;; 50-at-point.el --- At point actions
+
+;; Time-stamp: <2016-06-24 11:31:34>
+;; Copyright (C) 2016 Pierre Lecocq
+
+;;; Commentary:
+
+;;; Code:
+
+(defvar se-url-fmt "https://www.google.com/?q=%s")
+
+(defun pl-thing-at-point-or-region (qualifier)
+  "Get thing at point according to QUALIFIER or the region."
+  (if (use-region-p)
+      (buffer-substring-no-properties (region-beginning) (region-end))
+    (thing-at-point qualifier)))
+
+(defun pl-browse-at-point ()
+  "Browse url or region at point."
+  (interactive)
+  (browse-url (pl-thing-at-point-or-region 'url)))
+
+(defun pl-search-at-point ()
+  "Search word or region at point."
+  (interactive)
+  (browse-url (format se-url-fmt (pl-thing-at-point-or-region 'word))))
+
+(defun pl-documentation-at-point ()
+  "Documentation for word or region at point."
+  (interactive)
+  (let ((fmt (cond ((string= major-mode "lisp-mode") "http://lispdoc.com/?q=%s&search=Basic+search")
+                   ((string= major-mode "ruby-mode") "http://apidock.com/ruby/search?query=%s&commit=Search")
+                   ((string= major-mode "php-mode") "http://php.net/%s")
+                   (t se-url-fmt))))
+    (browse-url (format fmt (pl-thing-at-point-or-region 'word)))))
+
+;;; 50-at-point.el ends here
