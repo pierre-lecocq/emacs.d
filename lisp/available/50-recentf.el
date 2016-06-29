8ac044dd13c27ea47461b64b9a7ea3c8524da571
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-06-29 10:34:32 +0200

    Fix recentf

--- lisp/available/50-recentf.el
+++ lisp/available/50-recentf.el
@@ -1,6 +1,6 @@
 ;;; 50-recentf.el --- Recentf
 
-;; Time-stamp: <2016-06-27 12:48:43>
+;; Time-stamp: <2016-06-29 10:32:13>
 ;; Copyright (C) 2016 Pierre Lecocq
 
 ;;; Commentary:
@@ -15,7 +15,7 @@
       recentf-max-menu-items 50
       recentf-keep '(file-remote-p file-readable-p))
 
-(add-to-list 'recentf-exclude packages-dir)
+(add-to-list 'recentf-exclude package-user-dir)
 (add-to-list 'recentf-exclude files-dir)
 
 ;;; 50-recentf.el ends here
