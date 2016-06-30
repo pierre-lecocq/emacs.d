18001258c5f9a91ff914d09ee4c673469f097922
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-06-30 09:10:15 +0200

    Remove the 64k file size limit since FTL has files big as fuck

--- lisp/etc/available/01-ffip.el
+++ lisp/etc/available/01-ffip.el
@@ -1,6 +1,6 @@
 ;;; 01-ffip.el --- Find file in project
 
-;; Time-stamp: <2016-06-29 10:56:30>
+;; Time-stamp: <2016-06-30 09:05:23>
 ;; Copyright (C) 2016 Pierre Lecocq
 
 ;;; Commentary:
@@ -16,7 +16,7 @@
   (setq ffip-prefer-ido-mode t)
   (when (or (ffip-current-full-filename-match-pattern-p "\\(/www/fotolia\\)")
             (ffip-current-full-filename-match-pattern-p "\\(/www/adobestock\\)"))
-    (setq-local ffip-find-options "-not -size +64k")
+    ;; (setq-local ffip-find-options "-not -size +64k")
     (setq-local ffip-patterns '("*.php" "*.js" "*.css"))
     (setq-local ffip-prune-patterns '("*/.git/*"
                                       "*/vendor/*"
