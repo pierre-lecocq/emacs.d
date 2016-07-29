c27ad18990054fbf77f414b63ada1c9516f930a4
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-07-29 11:26:42 +0200

    Projector - project activation and deactivation

--- lisp/available/99-projector.el
+++ lisp/available/99-projector.el
@@ -1,6 +1,6 @@
 ;;; 99-projector.el --- Project manager
 
-;; Time-stamp: <2016-07-29 11:15:00>
+;; Time-stamp: <2016-07-29 11:26:17>
 ;; Copyright (C) 2016 Pierre Lecocq
 
 ;;; Commentary:
@@ -10,7 +10,6 @@
 (defvar projector-file ".projector.el")
 
 (defvar projector--dir nil)
-
 (defvar projector--missed-count 0)
 (defvar projector--missed-max 5)
 
@@ -29,35 +28,25 @@
              (not (string= dname projector--dir))
              (yes-or-no-p "Do you want to activate projector? "))
     (setq projector--dir dname)
-    (message " - Activated")))
+    (message "Projector has been activated")))
+
+(defun projector--check-and-deactivate ()
+  "Check if the user wants to deactiavte projector after missing hits."
+  (if (< projector--missed-count projector--missed-max)
+      (incf projector--missed-count)
+    (when (yes-or-no-p "Do you want to deactivate projector? ")
+      (setq projector--dir nil)
+      (setq projector--missed-count 0)
+      (message "Projector has been deactivated"))))
 
 (defun projector--hook ()
   "Projector hook."
   (let ((dname (file-name-directory buffer-file-name))
         (fname (file-name-nondirectory buffer-file-name)))
-
-    (message " - Dir = %s" projector--dir)
-    (message " - Fname = %s" fname)
-    (message " - Dname = %s" dname)
-
     (projector--check-and-activate fname dname)
-
-    (if (projector--activated-p)
-        (if (projector--in-scope-p dname)
-            (message "I am in the project scope")
-          (if (< projector--missed-count projector--missed-max)
-              (progn
-                (incf projector--missed-count)
-                (message "I am NOT in the project scope (%d)" projector--missed-count))
-            (when (yes-or-no-p "Do you want to deactivate projector? ")
-              (setq projector--dir nil)
-              (setq projector--missed-count 0)
-              (message " - Deactivated")
-              )
-            ))
-      )
-
-    ))
+    (if (and (projector--activated-p)
+             (not (projector--in-scope-p dname)))
+        (projector--check-and-deactivate))))
 
 (add-hook 'find-file-hook 'projector--hook)
 
