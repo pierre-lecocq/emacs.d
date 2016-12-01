31f7889f1f9066f6cb4baa595a3e3f50cf9b1037
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-12-01 18:47:34 +0100

    More tabbar

--- lisp/available/init-tabbar.el
+++ lisp/available/init-tabbar.el
@@ -1,6 +1,6 @@
 ;;; init-tabbar.el --- Tabbar
 
-;; Time-stamp: <2016-12-01 14:17:24>
+;; Time-stamp: <2016-12-01 18:42:15>
 ;; Copyright (C) 2016 Pierre Lecocq
 
 ;;; Commentary:
@@ -12,17 +12,25 @@
           (tabbar-mode 1)
           (setq tabbar-use-images nil)
           (set-face-attribute 'tabbar-default nil
-                              :background "grey20"
-                              :foreground "grey40"
-                              :box '(:line-width 6 :color "grey20"))
+                              :background "grey22"
+                              :foreground "grey50"
+                              :box '(:line-width 6 :color "grey22"))
+          (set-face-attribute 'tabbar-unselected nil
+                              :background "grey22"
+                              :foreground "grey50"
+                              :box '(:line-width 6 :color "grey22"))
           (set-face-attribute 'tabbar-selected nil
                               :background "grey13"
-                              :foreground "grey70"
+                              :foreground "grey80"
                               :box '(:line-width 6 :color "grey13"))
-          (set-face-attribute 'tabbar-unselected nil
-                              :background "grey20"
-                              :foreground "grey40"
-                              :box '(:line-width 6 :color "grey20"))
+          (set-face-attribute 'tabbar-modified nil
+                              :background "grey22"
+                              :foreground "red"
+                              :box '(:line-width 6 :color "grey22"))
+          (set-face-attribute 'tabbar-selected-modified nil
+                              :background "grey13"
+                              :foreground "red"
+                              :box '(:line-width 6 :color "grey13"))
           (set-face-attribute 'tabbar-button nil
                               :box nil)
           (set-face-attribute 'tabbar-separator nil
