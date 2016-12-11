c7fa87d8af9c24dda784ca5a0c1650bc078c34af
Pierre Lecocq <pierre.lecocq@gmail.com>
2016-12-11 18:14:46 +0100

    Add robe pkg for Ruby

--- lisp/available/lang-ruby.el
+++ lisp/available/lang-ruby.el
@@ -1,6 +1,6 @@
 ;;; lang-ruby.el --- Ruby
 
-;; Time-stamp: <2016-10-14 08:47:58>
+;; Time-stamp: <2016-12-11 18:13:11>
 ;; Copyright (C) 2015 Pierre Lecocq
 
 ;;; Commentary:
@@ -10,6 +10,9 @@
 (use-package ruby-mode :ensure t)
 
 (use-package inf-ruby :ensure t)
+
+(use-package robe :ensure t
+  :config (push 'company-robe company-backends))
 
 (use-package rubocop :ensure t)
 
