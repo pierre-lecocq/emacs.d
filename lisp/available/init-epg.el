b5c28a7f159e208777745f73369f8c2cdb735feb
Pierre Lecocq <pierre.lecocq@gmail.com>
2017-01-11 10:05:37 +0100

    Add EasyPG config

---
+++ lisp/available/init-epg.el
@@ -0,0 +1,16 @@
+;;; init-epg.el --- EasyPG
+
+;; Time-stamp: <2017-01-11 10:02:10>
+;; Copyright (C) 2017 Pierre Lecocq
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'epa-file)
+
+(epa-file-enable)
+
+(provide 'init-epg)
+
+;;; init-epg.el ends here
