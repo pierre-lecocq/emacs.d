28e394329b2c02f3abd67416e17ce9059a0ec1e7
Pierre Lecocq <lecocq@adobe.com>
2019-06-05 09:02:18 +0200

    Toggle shell

--- modules/feat-shell.el
+++ modules/feat-shell.el
@@ -1,19 +1,28 @@
 ;;; feat-shell.el --- Shell feature -*- lexical-binding: t; -*-
 
-;; Time-stamp: <2019-06-04 22:48:54>
+;; Time-stamp: <2019-06-05 09:01:34>
 ;; Copyright (C) 2019 Pierre Lecocq
 
 ;;; Commentary:
 
 ;;; Code:
 
-(defadvice ansi-term (before force-bash)
-  "Advice BEFORE ansi term with FORCE-BASH."
-  (interactive (list "/bin/bash")))
+;; (defadvice ansi-term (before force-bash)
+;;   "Advice BEFORE ansi term with FORCE-BASH."
+;;   (interactive (list "/bin/bash")))
+;; (ad-activate 'ansi-term)
 
-(ad-activate 'ansi-term)
+(defun toggle-shell ()
+  "Toggle shell."
+  (interactive)
+  (let ((shell-buf (get-buffer "*ansi-term*")))
+    (if shell-buf
+        (if (equal (current-buffer) shell-buf)
+            (switch-to-buffer (other-buffer))
+          (switch-to-buffer shell-buf))
+      (ansi-term "/bin/bash"))))
 
-(global-set-key (kbd "<M-return>") 'ansi-term)
+(global-set-key (kbd "<M-return>") 'toggle-shell)
 
 (provide 'feat-shell)
 
