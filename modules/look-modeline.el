224613a1f8ec806ef3fdb85f687be629f2995e22
Pierre Lecocq <pierre.lecocq@gmail.com>
2019-07-26 10:48:06 -0700

    Include project name in simple modeline

--- modules/look-modeline.el
+++ modules/look-modeline.el
@@ -1,6 +1,6 @@
 ;;; look-modeline.el --- Modeline feature -*- lexical-binding: t; -*-
 
-;; Time-stamp: <2019-07-26 09:01:03>
+;; Time-stamp: <2019-07-26 10:47:16>
 ;; Copyright (C) 2019 Pierre Lecocq
 
 ;;; Commentary:
@@ -14,7 +14,10 @@
        (setq-default mode-line-format nil))
 
       ((eq host-modeline-type 'simple)
-       (setq-default mode-line-format '(" %* %f [%l:%c %p] %m")))
+       (setq-default mode-line-format
+                     '(" %* " (:eval (if (projectile-project-p)
+                                         (concat "@" (projectile-project-name) "/" (replace-regexp-in-string (projectile-project-root) "" buffer-file-name))
+                                       (buffer-file-name))) " [%l:%c %p] %m")))
 
       ((eq host-modeline-type 'full)
        (defgroup host-modeline nil
