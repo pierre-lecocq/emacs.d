551dfaeccc902ccd6fd8f3167d553550488a750c
Pierre Lecocq <pierre.lecocq@gmail.com>
2019-10-01 23:17:33 +0200

    Remove imenu autoresize

--- modules/init-imenu.el
+++ modules/init-imenu.el
@@ -1,6 +1,6 @@
 ;;; init-imenu.el --- Imenu init -*- lexical-binding: t; -*-
 
-;; Time-stamp: <2019-10-01 15:33:39>
+;; Time-stamp: <2019-10-01 23:17:07>
 ;; Copyright (C) 2019 Pierre Lecocq
 
 ;;; Commentary:
@@ -11,7 +11,6 @@
   :bind ("C-c i m" . imenu))
 
 (use-package imenu-list :ensure t
-  :init (setq imenu-list-auto-resize t)
   :config (imenu-list-minor-mode)
   :bind ("C-c i l" . imenu-list))
 
