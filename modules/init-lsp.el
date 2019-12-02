c8fe6656602e5428d57cf4627f605dbc244657cf
Pierre Lecocq <pierre.lecocq@gmail.com>
2019-12-02 13:51:58 +0100

    Add basic LSP support

---
+++ modules/init-lsp.el
@@ -0,0 +1,48 @@
+;;; init-lsp.el --- LSP support -*- lexical-binding: t; -*-
+
+;; Time-stamp: <2019-12-02 13:50:55>
+;; Copyright (C) 2019 Pierre Lecocq
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package lsp-mode :ensure t
+  :hook (js2-mode . lsp)
+  :commands lsp
+  :config (setq lsp-prefer-flymake nil))
+
+(use-package lsp-ui :ensure t
+  :commands lsp-ui-mode
+  :config (setq lsp-ui-flycheck-enable t
+                lsp-ui-flycheck-list-position 'right
+                lsp-ui-flycheck-live-reporting t
+                lsp-ui-doc-enable t
+                lsp-ui-doc-use-childframe t
+                lsp-ui-doc-position 'top
+                lsp-ui-doc-include-signature t
+                lsp-ui-peek-enable t
+                lsp-ui-peek-list-width 60
+                lsp-ui-peek-peek-height 25
+                lsp-ui-sideline-enable nil))
+
+(use-package company-lsp :ensure t
+  :commands company-lsp
+  :config (push 'company-lsp company-backends))
+
+;; (defun lsp-session ()
+;;   "Launch a LSP session."
+;;   (interactive)
+;;   (lsp)
+;;   (lsp-ui-mode 1)
+;;   (lsp-ui-imenu)
+;;   (lsp-ui-sideline-mode 1)
+;;   (lsp-ui-peek-mode 1)
+;;   (lsp-ui-doc-mode 1)
+;;   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions) ;; M-.
+;;   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references) ;; M-?
+;;   )
+
+(provide 'init-lsp)
+
+;;; init-lsp.el ends here
