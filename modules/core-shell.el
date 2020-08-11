394fbf60a9d3c7ba49d0ce232645ddc01db2ec04
Pierre Lecocq <pierre.lecocq@gmail.com>
2020-08-11 21:38:40 +0200

    Properly bind vterm

--- modules/core-shell.el
+++ modules/core-shell.el
@@ -1,6 +1,6 @@
 ;;; core-shell.el --- Shell -*- lexical-binding: t; -*-
 
-;; Time-stamp: <2020-06-23 16:06:06>
+;; Time-stamp: <2020-08-11 21:30:52>
 ;; Copyright (C) 2020 Pierre Lecocq
 
 ;;; Commentary:
@@ -8,14 +8,14 @@
 ;;; Code:
 
 (use-package vterm :ensure t
-  :bind ("C-RET" . (lambda ()
-                     (interactive)
-                     (let ((bname "vterm"))
-                       (if (string= (buffer-name) bname)
-                           (switch-to-buffer (other-buffer))
-                         (if (get-buffer bname)
-                             (switch-to-buffer bname)
-                           (vterm)))))))
+  :bind ("<C-return>" . (lambda ()
+                          (interactive)
+                          (let ((bname "vterm"))
+                            (if (string= (buffer-name) bname)
+                                (switch-to-buffer (other-buffer))
+                              (if (get-buffer bname)
+                                  (switch-to-buffer bname)
+                                (vterm)))))))
 
 (provide 'core-shell)
 
